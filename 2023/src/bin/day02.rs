a9187f5c5d34557eddb9f11eec4f632284aeee78
Lyr <lyr-7d1h@pm.me>
2023-12-03 01:09:55 +0100

    day2

--- 2023/src/bin/day02.rs
+++ 2023/src/bin/day02.rs
@@ -1,5 +1,6 @@
 use advent_of_code_2023::Aoc;
 
+// 25us
 fn part1(input: String) -> u32 {
     return input
         .lines()
@@ -11,13 +12,44 @@ fn part1(input: String) -> u32 {
             id = &id[0..id.len() - 1];
             let id = id.parse::<u32>().unwrap();
 
+            while let Some(n) = spaced.next() {
+                let n = n.parse::<u32>().unwrap();
+                let color = spaced.next().unwrap();
+                match color.chars().next().unwrap() {
+                    'r' if n > 12 => return None,
+                    'g' if n > 13 => return None,
+                    'b' if n > 14 => return None,
+                    _ => {}
+                };
+            }
+
             Some(id)
         })
         .sum();
 }
 
+// 40us
 fn part2(input: String) -> u32 {
-    todo!()
+    return input
+        .lines()
+        .map(|l| {
+            let mut spaced = l.split(" ").skip(2);
+
+            let mut max = (0, 0, 0);
+            while let Some(n) = spaced.next() {
+                let n = n.parse::<u32>().unwrap();
+                let color = spaced.next().unwrap();
+                match color.chars().next().unwrap() {
+                    'r' => max.0 = max.0.max(n),
+                    'g' => max.1 = max.1.max(n),
+                    'b' => max.2 = max.2.max(n),
+                    _ => {}
+                };
+            }
+
+            max.0 * max.1 * max.2
+        })
+        .sum();
 }
 
 fn main() {
